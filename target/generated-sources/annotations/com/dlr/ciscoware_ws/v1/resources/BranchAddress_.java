b3f9daf22bb2e80531b50b5d37d0490eae864240
Alex Reyes <me@alexjreyes.com>
2019-04-21 16:48:52 +0800

    Add Admin methods

--- target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/BranchAddress_.java
+++ target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/BranchAddress_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-21T16:04:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-21T16:37:33")
 @StaticMetamodel(BranchAddress.class)
 public class BranchAddress_ { 
 
--- target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/Order1_.java
+++ target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/Order1_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-21T16:04:41")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-21T16:37:33")
 @StaticMetamodel(Order1.class)
 public class Order1_ { 
 
