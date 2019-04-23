4551627b3562af0a7cd283ee23733ded301c6a72
Alex Reyes <me@alexjreyes.com>
2019-04-24 00:13:57 +0800

    Add branches to Orders GET methods

--- target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/CustomerAddress_.java
+++ target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/CustomerAddress_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-23T23:39:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-24T00:10:37")
 @StaticMetamodel(CustomerAddress.class)
 public class CustomerAddress_ { 
 
--- target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/User_.java
+++ target/generated-sources/annotations/com/dlr/ciscoware_ws/v1/resources/User_.java
@@ -8,7 +8,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-23T23:39:51")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-04-24T00:10:37")
 @StaticMetamodel(User.class)
 public class User_ { 
 
